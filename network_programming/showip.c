982a8a115a30ba80bd7e68c592e450b20af4e8e1
Souvik <Souvikc3030@gmail.com>
2023-12-13 03:22:43 +0530

    show ip

--- network_programming/showip.c
+++ network_programming/showip.c
@@ -10,5 +10,12 @@
 
 
 int main(int argc, char *argv[]) {
+  struct addrinfo hints, *res, *p;
+  int status;
+  char ipstr[INET6_ADDRSTRLEN];
 
+  if(argc != 2){
+    fprintf(stderr, "usage: showip hostname\n");
+    return 1;
+  }
 }
